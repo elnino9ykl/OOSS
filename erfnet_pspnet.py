a0e77b18594cee6c4706f69efc4ba9b9bad007ca
elnino9ykl <elnino@zju.edu.cn>
2020-02-19 13:05:45 +0100

    Update erfnet_pspnet.py

--- erfnet_pspnet.py
+++ erfnet_pspnet.py
@@ -18,8 +18,8 @@ class DownsamplerBlock (nn.Module):
 
         self.conv = nn.Conv2d(ninput, noutput-ninput, (3, 3), stride=2, padding=1, bias=True)
         self.pool = nn.MaxPool2d(2, stride=2)
-        self.bn = BatchNorm(noutput, eps=1e-3)
-        #self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+        #self.bn = BatchNorm(noutput, eps=1e-3)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
 
     def forward(self, input):
         output = torch.cat([self.conv(input), self.pool(input)], 1)
@@ -34,15 +34,15 @@ class non_bottleneck_1d (nn.Module):
 
         self.conv1x3_1 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1), bias=True)
 
-        self.bn1 = BatchNorm(chann, eps=1e-03)
-        #self.bn1 = nn.BatchNorm2d(chann, eps=1e-03)
+        #self.bn1 = BatchNorm(chann, eps=1e-03)
+        self.bn1 = nn.BatchNorm2d(chann, eps=1e-03)
 
         self.conv3x1_2 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1*dilated,0), bias=True, dilation = (dilated,1))
 
         self.conv1x3_2 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1*dilated), bias=True, dilation = (1, dilated))
 
-        self.bn2 = BatchNorm(chann, eps=1e-03)
-        #self.bn2 = nn.BatchNorm2d(chann, eps=1e-03)
+        #self.bn2 = BatchNorm(chann, eps=1e-03)
+        self.bn2 = nn.BatchNorm2d(chann, eps=1e-03)
 
         self.dropout = nn.Dropout2d(dropprob)
         
@@ -104,8 +104,8 @@ class UpsamplerBlock (nn.Module):
     def __init__(self, ninput, noutput):
         super().__init__()
         self.conv = nn.ConvTranspose2d(ninput, noutput, 3, stride=2, padding=1, output_padding=1, bias=True)
-        self.bn = BatchNorm(noutput, eps=1e-3)
-        #self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+        #self.bn = BatchNorm(noutput, eps=1e-3)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
 
     def forward(self, input):
         output = self.conv(input)
@@ -114,14 +114,14 @@ class UpsamplerBlock (nn.Module):
 
 class PSPDec(nn.Module):
 
-    def __init__(self, in_features, out_features, downsize, upsize=(64,64)):
+    def __init__(self, in_features, out_features, downsize, upsize=(64,128)):
         super(PSPDec,self).__init__()
 
         self.features = nn.Sequential(
             nn.AvgPool2d(downsize, stride=downsize),
             nn.Conv2d(in_features, out_features, 1, bias=False),
-            BatchNorm(out_features, momentum=.95),
-            #nn.BatchNorm2d(out_features, momentum=.95),
+            #BatchNorm(out_features, momentum=.95),
+            nn.BatchNorm2d(out_features, momentum=.95),
             nn.ReLU(inplace=True),
             nn.Upsample(size=upsize, mode='bilinear')
         )
@@ -140,8 +140,8 @@ class Decoder (nn.Module):
 
         self.final = nn.Sequential(
             nn.Conv2d(256, 256, 3, padding=1, bias=False),
-            BatchNorm(256, momentum=.95),
-            #nn.BatchNorm2d(256, momentum=.95),
+            #BatchNorm(256, momentum=.95),
+            nn.BatchNorm2d(256, momentum=.95),
             nn.ReLU(inplace=True),
             nn.Dropout(.1),
             #nn.Conv2d(256, num_classes, 1),
